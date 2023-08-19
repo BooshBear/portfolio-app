2bf1f3af195d7516b3d61f72016fd0fce0640d63
Dylan.Mailloux <Dsisson0505@gmail.com>
2023-08-19 13:21:30 -0700

    Finishing Layout for first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,225 @@
+'use client'
 import Image from 'next/image'
+import { useEffect, useState } from 'react';
+
+// I decided to go for the gamer/programmer kinda vibe so I should add a few animations.
 
 export default function Home() {
+  const [showAboutMe, setShowAboutMe] = useState(false);
+  const [typedText, setTypedText] = useState('');
+
+  useEffect(() => {
+    if (showAboutMe) {
+      const fullText = "Hi my name is Dylan and this is my website. Your welcome to look around. I am currently looking for a job and freelancing to help me stay alive in this time in age. So if you have any questions or offers just send me a email and I will try and respond in atleast a day. Oh also there's a plentyful of projects at the bottom that you can look at if you want to see my skills in action. Email: Dsisson0505@gmail.com"; // Replace this with your desired text
+      let currentIndex = 0;
+
+      const typingInterval = setInterval(() => {
+        setTypedText(fullText.slice(0, currentIndex));
+        currentIndex++;
+
+        if (currentIndex > fullText.length) {
+          clearInterval(typingInterval);
+        }
+      }, 65); // Adjust the typing speed here
+    }
+  }, [showAboutMe]);
+
+
+  function aboutMe() {
+    return <div className='bg-green-400 min-w-[50rem] p-4 rounded-lg text-black max-w-[80rem]'>
+      <h1>{typedText}</h1>
+    </div>;
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center justify-around">
+      {/* Beginning animation line */}
+      <div className='absolute translate-x-[100%] bg-black h-[136vh] w-full left-0 animate-left_to_right'>
+        <div className='relative left-0 min-h-full w-3 tv-static z-1 animate-left_to_right'></div>
+      </div>
+      
+      
+      {/* About Me */}
+      <section className='flex flex-col justify-center items-center h-[15rem] w-full'>
+        <h1>To Learn More About Me</h1>
+        <button className='hover:animate-wiggle p-2' onClick={() => setShowAboutMe(true)}>
+          Click <span>Here</span>
+        </button>
+
+        <div>
+          {showAboutMe && aboutMe()}
         </div>
-      </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      {/* Freelancing pricing area */}
+      <section className='flex flex-col justify-center items-center w-full'>
+        <table className='table-auto border-separate border-spacing-x-60 border-spacing-y-2 border text-center p-2 border-green-400 border-solid rounded-sm'>
+          <thead>
+            <th colSpan={3} className='p-4'><h1>Freelancing Package Options</h1></th>
+          </thead>
+          <thead>
+            <th className='w-[80px] p-2 border border-green-400 bg-gray-900'>Basic</th>
+            <th className='w-[80px] p-2 bg-green-400 border border-gray-800 text-black'>Standard</th>
+            <th className='w-[80px] p-2 border border-green-400 bg-gray-900 '>Premium</th>
+          </thead>
+          {/* # of Pages */}
+          <tr>
+            <td colSpan={3}><h4>Number of Pages</h4></td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>3</td>
+            <td>6</td>
+          </tr>
+          {/* Revisions */}
+          <tr>
+            <td colSpan={3}><h4>Revisions</h4></td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>3</td>
+            <td>6</td>
+          </tr>
+          {/* Content Upload */}
+          <tr>
+            <td colSpan={3}><h4>Content Upload</h4></td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>3</td>
+            <td>6</td>
+          </tr>
+          {/* Plugins/Extensions */}
+          <tr>
+            <td colSpan={3}><h4>Plugins/Extensions</h4></td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>3</td>
+            <td>8</td>
+          </tr>
+          {/* Full Website */}
+          <tr>
+            <td colSpan={3}><h4>Full Website</h4></td>
+          </tr>
+          <tr>
+            <td>/</td>
+            <td>/</td>
+            <td>/</td>
+          </tr>
+          {/* E-commerce Functionality */}
+          <tr>
+            <td colSpan={3}><h4>E-commerce Functionality</h4></td>
+          </tr>
+          <tr>
+            <td>x</td>
+            <td>/</td>
+            <td>/</td>
+          </tr>
+          {/* Number of products */}
+          <tr>
+            <td colSpan={3}><h4>Number of Products</h4></td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>3</td>
+            <td>8</td>
+          </tr>
+          {/* Payment Processing */}
+          <tr>
+            <td colSpan={3}><h4>Payment Processing</h4></td>
+          </tr>
+          <tr>
+            <td>x</td>
+            <td>/</td>
+            <td>/</td>
+          </tr>
+          {/* Opt-In Form */}
+          <tr>
+            <td colSpan={3}><h4>Opt-In Form</h4></td>
+          </tr>
+          <tr>
+            <td>x</td>
+            <td>x</td>
+            <td>/</td>
+          </tr>
+          {/* Autoresponder Integration */}
+          <tr>
+            <td colSpan={3}><h4>Autoresponder Integration</h4></td>
+          </tr>
+          <tr>
+            <td>x</td>
+            <td>x</td>
+            <td>/</td>
+          </tr>
+          {/* Speed optimization */}
+          <tr>
+            <td colSpan={3}><h4>Speed optimization</h4></td>
+          </tr>
+          <tr>
+            <td>x</td>
+            <td>/</td>
+            <td>/</td>
+          </tr>
+          {/* Hosting Setup */}
+          <tr>
+            <td colSpan={3}><h4>Hosting Setup</h4></td>
+          </tr>
+          <tr>
+            <td>x</td>
+            <td>/</td>
+            <td>/</td>
+          </tr>
+          {/* Social Media Icons */}
+          <tr>
+            <td colSpan={3}><h4>Social Media Icons</h4></td>
+          </tr>
+          <tr>
+            <td>x</td>
+            <td>/</td>
+            <td>/</td>
+          </tr>
+          {/* Liscensed Images //From Shuterstock */}
+          <tr>
+            <td colSpan={3}><h4>Liscened Images</h4></td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>3</td>
+            <td>6</td>
+          </tr>
+          {/* Prices */}
+          <tr>
+            <td colSpan={3}><h4>Prices</h4></td>
+          </tr>
+          <tr>
+            <td>$80</td>
+            <td>$260</td>
+            <td>$450</td>
+          </tr>
+          {/* Buttons */}
+          <tr>
+            <td><button className='w-[80px] p-2 border border-green-400 bg-gray-900'>Buy</button></td>
+            <td><button className='w-[80px] p-2 bg-green-400 border border-gray-800 text-black'>Buy</button></td>
+            <td><button className='w-[80px] p-2 border border-green-400 bg-gray-900'>Buy</button></td>
+          </tr>
+        </table>
+      </section>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {/* My Projects/Experience */}
+      <section className='flex flex-col justify-center items-center h-[20rem] w-full'>
+        <p className='p-2'>My Projects area</p>
+        <div className='flex gap-6 bg-slate-200 border-4 border-green-400 rounded-md w-[80rem] max-w-[80rem] p-4 text-black'>
+          <div>
+            <h2 className='text-center'>Catering Business</h2>
+            <img placeholder='thumbnail' src="" className='w-[200px] h-[200px]'></img>
+          </div>
+          <div>
+            <h2 className='text-center'>Todo App</h2>
+            <img placeholder='thumbnail' src="" className='w-[200px] h-[200px]'></img>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,20 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        left_to_right: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        },
+      },
+      animation: {
+        left_to_right: 'left_to_right 5s',
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      }
     },
   },
   plugins: [],
